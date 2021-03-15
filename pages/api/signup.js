65dbd20044f99f595abd7516b4dc5dd4e9994cd6
mojpm <itsjoelpm@gmail.com>
2021-03-15 22:25:27 +0300

    inplement signup functionality

---
+++ pages/api/signup.js
@@ -0,0 +1,27 @@
+import nc from "next-connect";
+
+import { insertUser } from "~/db/user";
+import { all } from "~/middlewares/index";
+
+const handler = nc();
+const bcrypt = require("bcrypt");
+
+handler.use(all);
+
+handler.post(async (req, res) => {
+  const { userName, email } = req.body;
+
+  // hash the password
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
+
+  const user = await insertUser(req.db, {
+    userName,
+    email,
+    password,
+  });
+
+  return res.json({ user });
+});
+
+export default handler;
