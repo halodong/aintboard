e2a3ecd97ea6a4b357bbe4d3e9e041f6eab1f031
mojpm <itsjoelpm@gmail.com>
2021-03-16 17:34:54 +0300

    login functionality

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,11 +2,6 @@ import nc from "next-connect";
 
 import { all } from "~/middlewares/index";
 
-/*
- * TODO
- * 1. Verify Token
- */
-
 const handler = nc();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -16,32 +11,48 @@ handler.use(all);
 handler.post(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await req.db.collections("users").findOne({ email: email }); //returns collections(...).findOne is not a function for some reason
+  const user = await req.db.collection("users").findOne({ email: email });
   if (!user) return res.status(400).json({ error: "Email is wrong" });
 
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword)
-    return res.status(400).json({ error: "Password is wrong" });
+  if (user) {
+    const userId = user._id,
+      userEmail = user.email,
+      userPassword = user.password;
 
-  const token = jwt.sign(
-    {
-      name: user.userName,
-      id: user._id,
-    },
-    process.env.TOKEN_SECRET
-  );
-  res.header("auth-token", token).json({
-    error: null,
-    data: {
-      token,
-    },
-  });
+    /* Check and compare password */
+    bcrypt.compare(password, userPassword).then((isMatch) => {
+      if (isMatch) {
+        //JWT Payload
+        const payload = {
+          id: userId,
+          email: userEmail,
+        };
 
-  return res.json({
-    error: null,
-    data: {
-      message: "Login successful",
-    },
-  });
+        // Sign token
+        jwt.sign(
+          payload,
+          process.env.TOKEN_SECRET,
+          {
+            expiresIn: 31556926, // 1 year in seconds
+          },
+          (err, token) => {
+            res.status(200).json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
+
+        return res.json({
+          data: {
+            message: "Login Successful",
+          },
+        });
+      } else {
+        res.status(400).json({ status: "error", error: "Password incorrect" });
+      }
+    });
+  }
 });
+
 export default handler;
