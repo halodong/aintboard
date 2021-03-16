5c28eaf152ad6962c36e28a4c764ce6c3a4479ae
mojpm <itsjoelpm@gmail.com>
2021-03-16 17:33:40 +0300

    add nanoid

--- db/user.js
+++ db/user.js
@@ -1,7 +1,10 @@
+import { nanoid } from "nanoid";
+
 export async function insertUser(db, { userName, email, password }) {
   return db
     .collection("users")
     .insertOne({
+      _id: nanoid(12),
       userName,
       email,
       password,
