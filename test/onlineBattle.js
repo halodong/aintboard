dc8d2eab0552f8e808a4f97b8cb0aa99dbb950ae
mojpm <itsjoelpm@gmail.com>
2021-03-22 14:50:20 +0300

    test creation of online battle

---
+++ test/onlineBattle.js
@@ -0,0 +1,36 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+require("dotenv").config();
+
+chai.use(chaiHttp);
+
+let expect = chai.expect;
+
+describe("Create Online Battle /POST /api/online-battles", () => {
+  it("it should create an online battle", (done) => {
+    chai
+      .request(process.env.LOCAL_URL)
+      .post("/api/online-battles")
+      .send({
+        battleName: "Battle of the legends",
+        boardGameName: "Boardie",
+        bggId: 54321,
+        details: "This is a battle for the legends",
+        eventStartDate: "4-05-2021",
+        eventEndDate: "5-05-2021",
+      })
+      .end((err, res) => {
+        if (err) done();
+        expect(res.status).to.equal(200);
+        expect(res.body.battle.battleName).to.equal("Battle of the legends");
+        expect(res.body.battle.boardGameName).to.equal("Boardie");
+        expect(res.body.battle.bggId).to.equal(54321);
+        expect(res.body.battle.details).to.equal(
+          "This is a battle for the legends"
+        );
+        expect(res.body.battle.eventStartDate).to.equal("4-05-2021");
+        expect(res.body.battle.eventEndDate).to.equal("5-05-2021");
+        done();
+      });
+  });
+});
