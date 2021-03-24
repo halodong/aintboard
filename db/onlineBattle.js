ff02fd38b0aaec776fe5060b18b37f4da7192206
mojpm <itsjoelpm@gmail.com>
2021-03-24 13:57:43 +0300

    move insertEntry method to onlineBattle

--- db/onlineBattle.js
+++ db/onlineBattle.js
@@ -18,3 +18,22 @@ export async function insertBattle(
     })
     .then(({ ops }) => ops[0]);
 }
+
+export async function insertValidEntry(
+  db,
+  { userId, battleId, score, message, googleLink, verifiedStatus }
+) {
+  return db
+    .collection("user_ob_entry")
+    .insertOne({
+      _id: nanoid(12),
+      userId,
+      battleId,
+      score,
+      message,
+      googleLink,
+      verifiedStatus,
+      createdAat: new Date(),
+    })
+    .then(({ ops }) => ops[0]);
+}
