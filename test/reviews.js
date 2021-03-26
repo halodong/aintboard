f07101f3093fedc5095006b1d342cd36d3e9f780
Faith Morante <faith.morante@gmail.com>
2021-03-26 16:56:08 +0800

    #61 reviews inmemory db test

--- test/reviews.js
+++ test/reviews.js
@@ -1,51 +1,56 @@
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-require("dotenv").config();
-
-chai.use(chaiHttp);
-
-let expect = chai.expect;
+import { insertReview } from "../db/reviews";
+const chai = require("chai");
+const expect = chai.expect;
+const dbHandler = require("./db-handler");
 
 describe("Reviews", () => {
-  describe("/POST /api/reviews", () => {
-    it("it should make a new review", (done) => {
-      chai
-        .request(process.env.LOCAL_URL)
-        .post("/api/reviews")
-        .send({
-          userId: 1,
-          bgId: 121,
-          reviewText: "3123123",
-          reviewStatusId: 1,
-          reviewType: "review",
-        })
-        .end((err, res) => {
-          if (err) done();
-          expect(res.status).to.equal(200);
-          expect(res.body.response.data.review.reviewText).to.equal("3123123");
-          expect(res.body.response.data.review.reviewStatusId).to.equal(1);
-          expect(res.body.response.data.review.reviewType).to.equal("review");
-          done();
-        });
+  let db;
+
+  before(async () => {
+    try {
+      db = await dbHandler.connect();
+      await dbHandler.clearDatabase();
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  after(async () => {
+    await dbHandler.clearDatabase();
+    await dbHandler.closeDatabase();
+  });
+
+  it("should make a new review", async () => {
+    let res = await insertReview(db, {
+      userId: 1,
+      bgId: 121,
+      reviewText: "3123123",
+      reviewStatusId: 1,
+      reviewType: "review",
     });
 
-    it("it should make a new strategy", (done) => {
-      chai
-        .request(process.env.LOCAL_URL)
-        .post("/api/reviews")
-        .send({
-          userId: 1,
-          bgId: 121,
-          reviewText: "3123123",
-          reviewStatusId: 1,
-          reviewType: "strategy",
-        })
-        .end((err, res) => {
-          if (err) done();
-          expect(res.status).to.equal(200);
-          expect(res.body.response.data.review.reviewType).to.equal("strategy");
-          done();
-        });
+    expect(res.success).to.equal(true);
+    expect(res.response.data.review.userId).to.equal(1);
+    expect(res.response.data.review.bgId).to.equal(121);
+    expect(res.response.data.review.reviewText).to.equal("3123123");
+    expect(res.response.data.review.reviewStatusId).to.equal(1);
+    expect(res.response.data.review.reviewType).to.equal("review");
+  });
+
+  it("should make a new strategy", async () => {
+    let res = await insertReview(db, {
+      userId: 1,
+      bgId: 121,
+      reviewText: "3123123",
+      reviewStatusId: 1,
+      reviewType: "strategy",
     });
+
+    expect(res.success).to.equal(true);
+    expect(res.response.data.review.userId).to.equal(1);
+    expect(res.response.data.review.bgId).to.equal(121);
+    expect(res.response.data.review.reviewText).to.equal("3123123");
+    expect(res.response.data.review.reviewStatusId).to.equal(1);
+    expect(res.response.data.review.reviewType).to.equal("strategy");
   });
 });
