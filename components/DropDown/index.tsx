af50b2bb5c07f27b2358762efe881e9cc625798f
glendell03 <dellbringino@gmail.com>
2021-03-29 20:59:12 +0800

    #40 Challenges page

--- components/DropDown/index.tsx
+++ components/DropDown/index.tsx
@@ -2,49 +2,6 @@ import { DropDownContainer } from "./styled";
 
 import Select from "react-select";
 
-const options = [
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-  "chess1",
-  "chess2",
-  "chess3",
-  "chess4",
-];
-
 const DropDown = ({ placeHolder, isName = false }: Props) => {
   return (
     <DropDownContainer isName={isName}>
--- components/DropDown/styled.ts
+++ components/DropDown/styled.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const DropDownContainer = styled.div<DropDownContainerProps>`
   width: ${(props) => (props.isName ? "20rem" : "12rem")};
   margin: 0 0.5rem;
+
+  .select {
+    font-family: ${(props) => props.theme.fonts.quicksandReg};
+  }
 `;
 
 type DropDownContainerProps = {
