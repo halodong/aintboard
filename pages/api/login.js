4ccb65ce1393d357112cdc937a997d2ab5eb452d
Faith Morante <faith.morante@gmail.com>
2021-04-03 19:50:11 +0800

    use axios for frontend http calls

--- pages/api/login.js
+++ pages/api/login.js
@@ -8,7 +8,7 @@ const handler = nc();
 handler.use(all);
 
 handler.post(async (req, res) => {
-  const { email = "", username = "", password } = JSON.parse(req.body);
+  const { email = "", username = "", password } = req.body;
 
   const user = await authenticateUser(req.db, {
     email,
