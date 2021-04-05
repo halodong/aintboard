ae10c29678264682e18ed5005fa8a5b8188a4c0a
Aj Guintu <amyojosiah@gmail.com>
2021-04-05 11:00:21 +0800

    #82-about-us font

--- components/About/styled.ts
+++ components/About/styled.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const AboutWrapper = styled.div`
   background-color: ${(props) => props.theme.colors.dark};
 
+  font-family: ${(props) => props.theme.fonts.quicksandReg};
   color: white;
   padding: 0 2rem 2rem 2rem;
   text-align: center;
