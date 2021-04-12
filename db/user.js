3d451bbb3fe1ac85c56f06930f06883d7543ea44
glendell03 <dellbringino@gmail.com>
2021-04-13 00:49:46 +0800

    #99 Admin Login Page

--- db/user.js
+++ db/user.js
@@ -111,6 +111,55 @@ export async function authenticateUser(db, { email, username, password }) {
   }
 }
 
+export async function authenticateAdmin(db, { email, username, password }) {
+  try {
+    const user = await db
+      .collection("users")
+      .findOne({ $or: [{ email }, { username }] });
+
+    if (!user) {
+      return getFailedResponse(
+        "error",
+        "db/user.js",
+        "Username or email is wrong"
+      );
+    }
+
+    if (user.role === "admin" || user.role === "community_manager") {
+      const userId = user._id,
+        userPassword = user.password;
+
+      const compare = await bcrypt.compare(password, userPassword);
+
+      if (compare) {
+        //JWT Payload
+        const payload = {
+          id: userId,
+        };
+
+        // Sign token
+        const jwtToken = await jwt.sign(payload, process.env.TOKEN_SECRET, {
+          expiresIn: process.env.TOKEN_EXPIRY,
+        });
+
+        delete user.password;
+
+        return getSuccessResponse({
+          message: "Login Successful",
+          data: {
+            user: user,
+            token: jwtToken,
+          },
+        });
+      } else {
+        return getFailedResponse("error", "db/user.js", "Password incorrect");
+      }
+    }
+  } catch (err) {
+    return getFailedResponse(err, "db/user.js", "User retrieval failed");
+  }
+}
+
 export const filterUsers = async (db, { first, filter, field }) => {
   try {
     let users = null;
