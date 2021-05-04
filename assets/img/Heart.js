a3fd95d5c3d7e8ae942dcfdb8263cd0858ba94db
rostum <rtampor>
2021-05-04 20:49:41 +0800

    #154 Change heart icon fixed

--- assets/img/Heart.js
+++ assets/img/Heart.js
@@ -1,31 +1,29 @@
-const Heart = ({ ...props }) => (
-  <svg
-    id="b9b3d76a-327c-4092-b45a-c0a7d464a93f"
-    data-name="Layer 2"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 684 550"
-  >
-    <polygon points="342.82 550 0 207 171.41 207 342.82 550" fill="#ed5aa1" />
-    <polygon
-      points="342.82 550 512.47 207 171.41 207 342.82 550"
-      fill="#fe7ab3"
-    />
-    <polygon points="342.82 550 684 207 512.47 207 342.82 550" fill="#c84f85" />
-    <polygon points="0 207 101.82 0 171.41 207 0 207" fill="#c55085" />
-    <polygon
-      points="101.82 0 268.94 0 342.82 78.94 171.41 207 101.82 0"
-      fill="#f35f91"
-    />
-    <polygon
-      points="171.41 207 342.82 78.94 512.47 207 171.41 207"
-      fill="#f7b0d2"
-    />
-    <polygon
-      points="580.82 0 416.05 0 343.21 78.94 512.21 207 580.82 0"
-      fill="#fe83af"
-    />
-    <polygon points="580.82 0 512.21 207 684 207 580.82 0" fill="#ed5f91" />
-  </svg>
-);
+import * as React from "react";
 
-export default Heart;
+function SvgComponent(props) {
+  return (
+    <svg
+      data-name="Layer 2"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 684 550"
+      {...props}
+    >
+      <path fill="#ed5aa1" d="M342.82 550L0 207h171.41l171.41 343z" />
+      <path fill="#fe7ab3" d="M342.82 550l169.65-343H171.41l171.41 343z" />
+      <path fill="#c84f85" d="M342.82 550L684 207H512.47L342.82 550z" />
+      <path fill="#c55085" d="M0 207L101.82 0l69.59 207H0z" />
+      <path
+        fill="#f35f91"
+        d="M101.82 0h167.12l73.88 78.94L171.41 207 101.82 0z"
+      />
+      <path fill="#f7b0d2" d="M171.41 207L342.82 78.94 512.47 207H171.41z" />
+      <path
+        fill="#fe83af"
+        d="M580.82 0H416.05l-72.84 78.94 169 128.06L580.82 0z"
+      />
+      <path fill="#ed5f91" d="M580.82 0l-68.61 207H684L580.82 0z" />
+    </svg>
+  );
+}
+
+export default SvgComponent;
