cf2b1fe2bd6636092532cac37c196f023f4cb4d0
Faith Morante <faith.morante@gmail.com>
2021-05-07 19:42:00 +0800

    #196 update review article

--- components/Reviews/ReviewArticlePage/styled.ts
+++ components/Reviews/ReviewArticlePage/styled.ts
@@ -6,6 +6,31 @@ export const ReviewArticlePageWrapper = styled.div`
   padding: 0 10rem 10rem 10rem;
 `;
 
+export const ReviewHeader = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 2rem 2rem 0;
+`;
+
+export const ReviewHeaderLeft = styled.div`
+  display: inline-block;
+`;
+
+export const SliderWrapper = styled.div`
+  display: inline-block;
+  width: 100%;
+  margin-left: 1rem;
+`;
+
+export const OverallRating = styled.span`
+  display: flex;
+  align-items: center;
+
+  span {
+    font-size: 2rem;
+  }
+`;
+
 export const LanguageContainer = styled.div`
   padding: 3rem 0;
 `;
