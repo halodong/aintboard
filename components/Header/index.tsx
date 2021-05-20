caec66ffdcfbf860084f224467a120b65415d8b9
glendell03 <dellbringino@gmail.com>
2021-05-20 14:14:28 +0800

    Fix Header Bug

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -109,10 +109,9 @@ export default function Header({
 
       {/* <Searchbar /> obsolete for now */}
 
-      {isUserPage ||
-        (isSettingsPage && (
-          <CenterTagline isUserPage>{children}</CenterTagline>
-        ))}
+      {(isUserPage || isSettingsPage) && (
+        <CenterTagline isUserPage>{children}</CenterTagline>
+      )}
 
       {(homepage || isBoardGamePage || isUserPage || isSettingsPage) && (
         <div className="homepage-bg-container">
