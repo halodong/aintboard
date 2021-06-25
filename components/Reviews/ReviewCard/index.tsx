36d1455e9abcc51e7a9eae0841f878d17099b63e
Glendell <dellbringino@gmail.com>
2021-06-25 14:54:06 +0800

    #230 Add fade animation when filtering

--- components/Reviews/ReviewCard/index.tsx
+++ components/Reviews/ReviewCard/index.tsx
@@ -22,53 +22,57 @@ import { ReviewData } from "types/types";
 import { createHTMLExcerpt } from "~/util/createHTML";
 import { REVIEWS_HOMEPAGE_COMPONENT, REVIEW_TYPE } from "~/util/constants";
 
+import FadeIn from "~/components/Common/FadeIn";
+
 export const ReviewCard = ({ data }: Props) => {
   const router = useRouter();
   return (
-    <ReviewCardContainer onClick={() => router.push(`/review/${data?.slug}`)}>
-      <BgImgWrapper>
-        {data.images?.length > 0 ? (
-          <Image alt="boardgame" src={data.images[0]} layout="fill" />
-        ) : (
-          <Image
-            alt="boardgame"
-            src="/img/landscape_default.png"
-            layout="fill"
-          />
-        )}
-      </BgImgWrapper>
-
-      <Avatar
-        iconType={data?.userData?.[0]?.avatar || ""}
-        from={REVIEWS_HOMEPAGE_COMPONENT}
-      />
-
-      <Username>{data?.userData?.[0]?.username}</Username>
-      <ReviewDate>{dayjs(data?.createdAt).format("MMM DD, YYYY")}</ReviewDate>
-      <ReviewContent>
-        <div
-          className="preview"
-          dangerouslySetInnerHTML={createHTMLExcerpt(data.content)}
-        ></div>
-      </ReviewContent>
-
-      <BottomWrapper>
-        <Bottom compType={data?.reviewType}>
-          {data?.reviewType === REVIEW_TYPE.REVIEW && (
-            <OverallRating rating={data.overallRating} paddingBottom="0" />
+    <FadeIn duration={450} delay={100}>
+      <ReviewCardContainer onClick={() => router.push(`/review/${data?.slug}`)}>
+        <BgImgWrapper>
+          {data.images?.length > 0 ? (
+            <Image alt="boardgame" src={data.images[0]} layout="fill" />
+          ) : (
+            <Image
+              alt="boardgame"
+              src="/img/landscape_default.png"
+              layout="fill"
+            />
           )}
+        </BgImgWrapper>
 
-          <BottomRight>
-            <h6>{capitalize(data.reviewType)}</h6>
+        <Avatar
+          iconType={data?.userData?.[0]?.avatar || ""}
+          from={REVIEWS_HOMEPAGE_COMPONENT}
+        />
 
-            <div className="socials">
-              <CommentIcon /> <span>100</span>
-              <HeartIcon /> <span>102</span>
-            </div>
-          </BottomRight>
-        </Bottom>
-      </BottomWrapper>
-    </ReviewCardContainer>
+        <Username>{data?.userData?.[0]?.username}</Username>
+        <ReviewDate>{dayjs(data?.createdAt).format("MMM DD, YYYY")}</ReviewDate>
+        <ReviewContent>
+          <div
+            className="preview"
+            dangerouslySetInnerHTML={createHTMLExcerpt(data.content)}
+          ></div>
+        </ReviewContent>
+
+        <BottomWrapper>
+          <Bottom compType={data?.reviewType}>
+            {data?.reviewType === REVIEW_TYPE.REVIEW && (
+              <OverallRating rating={data.overallRating} paddingBottom="0" />
+            )}
+
+            <BottomRight>
+              <h6>{capitalize(data.reviewType)}</h6>
+
+              <div className="socials">
+                <CommentIcon /> <span>100</span>
+                <HeartIcon /> <span>102</span>
+              </div>
+            </BottomRight>
+          </Bottom>
+        </BottomWrapper>
+      </ReviewCardContainer>
+    </FadeIn>
   );
 };
 
